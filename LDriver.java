1b075bb86832ed328fbe1f733d7b30e7f01e210a
Kiran Bhadury <kbhadury@ucdavis.edu>
2016-11-13 00:03:19 -0800

    Driver handles multiple iterations

--- LDriver.java
+++ LDriver.java
@@ -1,8 +1,16 @@
 import java.awt.Color;
+import java.util.Random;
+
 public class LDriver{
     public static void main(String[] args){
         LSystem sys = new LSystem();
+        Random rand = new Random();
         sys.getInput();
-        sys.draw(-250, -50, 90, 2, new Color(1,150,150), 1);
+        //draw(x, y, direction, width, color
+        for(int i = 0; i < 7; ++i){
+            int x = rand.nextInt(500)-250;
+            int y = rand.nextInt(100)-50;
+            sys.draw(x, y, 90, 2, new Color(1,150,150));
+        }
     }
 }
